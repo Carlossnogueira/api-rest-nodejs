0049a465cd36b5fa3bda11e4dd184baa9c7d6702
Carlossnogueira <carlosnogueira.tkdev@gmail.com>
2025-04-25 20:49:09 -0300

    utilizando de requests e testando com arquivo .http

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,29 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import {knex} from './database'
 
 const app = fastify()
 
 app.get('/hello', async() => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables;
+  const transaction = await knex('transactions').insert({
+    id: crypto.randomUUID(),
+    title: 'Transação de Teste',
+    amount:1000,
+  }).returning('*') // retorna todas as alterações
+  
+  return transaction
+})
+
+// 1. Apenas testando
+app.get('/return', async() =>{
+  const transactions = await knex('transactions').select('*')
+  return transactions
+})
+
+// 2. Testando também
+app.get('/especific', async () =>{
+  const especific = await knex('transactions').where('amount',1000).select('*')
+  return especific
 })
 
 app
