d701591c2cc1dfe0a9795c8f54a7a1a695844743
Carlossnogueira <carlosnogueira.tkdev@gmail.com>
2025-06-05 20:18:16 -0300

    adicionando rota de listagem de transações, resumo e listagem unica

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -7,7 +7,7 @@ declare module 'knex/types/tables'{
         transactions:{
             id:string
             title:string
-            amout:number
+            amount:number
             created_at:string
             session_id?:string
         }
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,34 @@ import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  
+  // listando todas as transações
+  app.get('/', async() =>{
+    const transactions = await knex('transactions').select()
+    return {
+      transactions
+    }
+  })
+
+  // resumo das transações
+  app.get('/sumary', async () =>{
+    const sumary = await knex('transactions').sum('amount', {as:'amout'}).first()
+    return { sumary }
+  })
+
+  // listando uma unica transação
+  app.get('/:id', async(request) =>{
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id',id).first()
+
+    return { transaction }
+  })
+
   app.post('/', async (request, reply) => {
     // formato da resiquição validado
     const createTransactionBodySchema = z.object({
