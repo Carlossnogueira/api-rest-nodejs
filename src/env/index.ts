ac9abba8b5a8805818baa78344a632203ff2f4af
Carlossnogueira <carlosnogueira.tkdev@gmail.com>
2025-06-20 22:09:10 -0300

    Organizando pacotes e adicionando o postgress ao projeto

--- src/env/index.ts
+++ src/env/index.ts
@@ -10,8 +10,9 @@ if (process.env.NODE_ENV === 'test') {
 // validando o .env
 const envSchema = z.object({
     NODE_ENV: z.enum(['development','test','production']).default('production'),
+    DATABASE_CLIENT: z.enum(['sqlite','pg']),
     DATABASE_URL: z.string(),
-    PORT: z.string().transform(Number)
+    PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
